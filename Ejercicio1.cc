e2d0bb8de713b2ee193ad598517c865790fc2ca0
leguzman14 <00003924@uca.edu.sv>
2024-10-31 23:00:42 -0600

    Ejercicio de repaso

--- Ejercicio1.cc
+++ Ejercicio1.cc
@@ -10,11 +10,17 @@ struct Libro {
   int year_publicacion;
 };
 
+void NuevoLibro(std::list<Libro>& libreria);
+bool CompararPorTitulo(const Libro& l1, const Libro& l2);
+void OrdenarLibrosPortitulo(std::list<Libro>& libreria);
+bool BuscarLibro(std::list<Libro> libreria, std::string libro_titulo_buscado);
+void MostrarLibros(std::list<Libro> libreria);
+
 int main() {
   std::list<Libro> libreria;
+  std::stack<Libro> pila_lectura;
   return 0;
 }
-
 
 void NuevoLibro(std::list<Libro>& libreria) {
   Libro libro;
@@ -27,19 +33,45 @@ void NuevoLibro(std::list<Libro>& libreria) {
   libreria.push_front(libro);
 }
 
-//Funcion para comparar por nombre
+// Funcion para comparar por nombre
 bool CompararPorTitulo(const Libro& l1, const Libro& l2) {
-    return l1.titulo < l2.titulo; // Comparar por nombre
+  return l1.titulo < l2.titulo;  // Comparar por nombre
 }
 
 // Función para ordenar los libros por titulo usando std::sort
 void OrdenarLibrosPortitulo(std::list<Libro>& libreria) {
-    libreria.sort(CompararPorTitulo); // Usar el método sort de std::list
+  libreria.sort(CompararPorTitulo);  // Usar el método sort de std::list
 }
 
 // Funcion para Buscar un libro
-void BuscarLibro(std::list<Libro> libreria, std::string libro_buscado)
-{
-      std::vector<Libro> vector_libros(libreria.begin(), libreria.end());
-
+bool BuscarLibro(std::list<Libro> libreria, std::string libro_titulo_buscado) {
+  // Ordenamos la lista
+  OrdenarLibrosPortitulo(libreria);
+  // Pasamos la lista a un vector
+  std::vector<Libro> vector_libros(libreria.begin(), libreria.end());
+  // Creamos temporalmente un objeto de tipo Libro para buscar en el vector
+  Libro libroBuscado{libro_titulo_buscado, "", 0};
+  // Aplicamos binary_search en el vector
+  return std::binary_search(vector_libros.begin(), vector_libros.end(),
+                            libroBuscado);
 }
+
+// Funcion para mostrar los libros
+void MostrarLibros(std::list<Libro> libreria) {
+  int i = 1;
+  while (!libreria.empty()) {
+    // Mostrar el titulo del elemento al frente de la lista
+    std::cout << i << " Titulo : " << libreria.front().titulo << std::endl;
+    // Eliminar el elemento al frente de la lista
+    libreria.pop_front();
+    i++;
+  }
+}
+
+// Extraer los elementos de la pila y agregarlos al inicio del vector
+    while (!pila.empty()) {
+        vector.insert(vector.begin(), pila.top()); // Agrega el elemento al inicio del vector
+        pila.pop(); // Elimina el elemento superior de la pila
+    }
+
+
